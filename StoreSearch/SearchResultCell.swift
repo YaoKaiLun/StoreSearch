4b5c01784acf08a1e076c08393c702af0058aabb
yaokailun <yaokailun@cvte.com>
2019-06-30 14:34:04 +0800

    Download images

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -14,6 +14,8 @@ class SearchResultCell: UITableViewCell {
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var artworkImageView: UIImageView!
     
+    var downloadTask: URLSessionDownloadTask?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
 
@@ -21,5 +23,26 @@ class SearchResultCell: UITableViewCell {
         selectedView.backgroundColor = UIColor(red: 20/255,
                                                green: 160/255, blue: 160/255, alpha: 0.5)
         selectedBackgroundView = selectedView
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        downloadTask?.cancel()
+        downloadTask = nil
+    }
+    
+    func configure(for result: SearchResult) {
+        nameLabel.text = result.name
+        
+        if result.artist.isEmpty {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = String(format: "%@ (%@)", result.artist, result.type)
+        }
+        
+        artworkImageView.image = UIImage(named: "Placeholder")
+        if let smallURL = URL(string: result.imageSmall) {
+            downloadTask = artworkImageView.loadImage(url: smallURL)
+        }
     }
 }
